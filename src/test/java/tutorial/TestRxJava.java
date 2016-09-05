5c17dbd00303c177f6faeb9c600582fb61ac697a
eric567 <gyc567@126.com>
2016-09-05 21:42:51 +0800

    Initial commit

---
+++ src/test/java/tutorial/TestRxJava.java
@@ -0,0 +1,66 @@
+package tutorial;
+
+import org.junit.Test;
+import rx.Observable;
+import rx.schedulers.Schedulers;
+
+/**
+ * Created by eric567 [email:gyc567@126.com]
+ * on 8/30/2016.
+ */
+public class TestRxJava {
+
+    @Test
+    public void test_hello_world() {
+
+        Observable.just("Hello, world!").map(s -> s + " -Dan")
+                .subscribe(s -> System.out.println(s));
+
+        Observable.just("Hello, RX")
+                .map(s -> s.hashCode())
+                .map(i -> Integer.toString(i))
+                .map(i -> i + 1)
+                .map(i -> i.toString())
+                .map(s -> s + "at")
+                .map(s -> s.toUpperCase())
+                .subscribe(i -> System.out.println(i));
+
+
+        String[] urls = {"url1", "url2"};
+        Observable.from(urls)
+                .subscribe(url -> System.out.println(url));
+
+
+        String[] urlls = {"url_a", "url_b", "url_c"};
+        query("key word")//search from text
+                .flatMap(urlss -> Observable.from(urlls))
+                .flatMap(url -> getTitle(url))
+                .subscribe(title -> System.out.println(title));
+
+
+        String[] urlls_filter = {"url_a", "url_b", "url_c"};
+        query("key word")//search from text
+                .flatMap(urlss -> Observable.from(urlls_filter))
+                .flatMap(url -> getTitle(url))
+                .filter(title -> !title.contains("a"))
+                .take(1)
+                .doOnNext(title -> saveTitle(title))
+                .subscribeOn(Schedulers.io())
+                .subscribe(title -> System.out.println(title));
+
+    }
+
+    public Observable<String> query(String txt) {
+        return Observable.just(txt);
+    }
+
+    public Observable<String> getTitle(String txt) {
+        return Observable.just(txt + "||title");
+    }
+
+    public void saveTitle(String txt) {
+        System.out.println(txt + "is saved!");
+    }
+
+
+}
