85b045ebc691a2fd779acd2915ca527b2aeada13
eric567 <gyc567@126.com>
2016-10-21 13:20:21 +0800

    add the test case for Formula

--- src/main/java/tutorial/Formula.java
+++ src/main/java/tutorial/Formula.java
@@ -1,5 +1,7 @@
 package tutorial;
 
+import java.util.List;
+
 /**
  * Created by eric567 [email:gyc567@126.com]
  * on 10/21/2016.
@@ -9,5 +11,11 @@ public interface Formula {
 
     default double sqrt(int a) {
         return Math.sqrt(a);
+    }
+    default List<String> sort(List<String> names)
+    {
+
+        names.sort((a, b) -> b.compareTo(a));
+        return names;
     }
 }
--- src/test/java/tutorial/FormulaTest.groovy
+++ src/test/java/tutorial/FormulaTest.groovy
@@ -1,9 +1,11 @@
 package tutorial
+
+import spock.lang.Specification
+
 /**
  * Created by eric567 [email:gyc567@126.com] 
  * on 10/21/2016.
  */
-import spock.lang.Specification
 class FormulaTest extends Specification {
     def "Calculate"() {
         given: "an instance of Formula "
@@ -38,4 +40,25 @@ class FormulaTest extends Specification {
 
 
     }
+
+    def "test collection in java8"()
+    {
+        given:"the list of String,and the instance of Formula "
+        Formula formula=new Formula() {
+            @Override
+            public double calculate(int a) {
+                return sqrt(a * 100);
+            }
+        };
+        List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+        when:"invoke th sort method "
+        List<String> rt=formula.sort(names)
+
+        then:"the first element of the list is anna"
+        rt.get(0).equals("xenia")==true
+        rt.get(3).equals("anna")==true
+
+
+    }
+
 }
